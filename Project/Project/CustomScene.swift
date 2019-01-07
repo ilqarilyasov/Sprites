4fedce3232a1e2b6f57a903b452826d5b5a284a5
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-07 17:11:35 -0600

    Add Xcode project. Add test text file

---
+++ Project/Project/CustomScene.swift
@@ -0,0 +1,19 @@
+//
+//  CustomScene.swift
+//  Project
+//
+//  Created by Ilgar Ilyasov on 1/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class CustomScebe: SKScene {
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first else { return }
+        
+        
+    }
+}
---
+++ Project/Project/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Ilgar Ilyasov on 1/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var skview: SKView!
+    var skscene: CustomScebe? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScebe(size: view.bounds.size)
+    }
+    
+}
+
